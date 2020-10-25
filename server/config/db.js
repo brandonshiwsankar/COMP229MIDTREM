48c7c130114331e46087eedc0d96ff88ddb3a755
Brandon Shiwsankar <brandon.shiwsankar@gmail.com>
2020-10-24 20:08:03 -0400

    connect to mongo online

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://brandon:yogebear10@midterm.mlikf.mongodb.net/<dbname>?retryWrites=true&w=majority"
 };
